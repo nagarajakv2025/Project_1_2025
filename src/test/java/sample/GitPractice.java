43c91b1a2aa79c500e428d0e9b9c1ea737660d5c
Nagaraja <nagaraj.kv@gail.com>
2025-04-01 16:14:01 +0530

    branch commit

---
+++ src/test/java/sample/GitPractice.java
@@ -0,0 +1,23 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class GitPractice {
+
+		public class Demo1 {
+
+			@Test
+
+			public void method()
+
+			{
+				System.out.println("pushed from git branch");
+			}
+
+	}
+		
+		
+
+	}
+
+
